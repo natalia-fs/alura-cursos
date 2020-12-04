48cef2fa32ecfe852359d3555459bd0f5289badc
Natalia <natalia-321s@outlook.com>
2020-12-04 11:59:46 -0300

    Cadastro de clientes e validação de cpf

--- Alura/Frontend/Javascript/fetchAPI/api/client.js
+++ Alura/Frontend/Javascript/fetchAPI/api/client.js
@@ -1,9 +1,23 @@
-listarClientes = () =>{
+const listarClientes = () =>{
     return fetch('http://localhost:4000/clientes')
     .then(response => {
         return response.json()
     })
     .then( json => {
         return json
+    })
+}
+
+const cadastrarClientes = (nome, cpf) => {
+    const json = JSON.stringify({nome: nome, cpf: cpf})
+    return fetch('http://localhost:4000/clientes/cliente', {
+        method: "post",
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: json
+    })
+    .then(response => {
+        return response.body
     })
 }
