bbbf383363c4942e9bf4ad49ee0e6d894ebde335
Natalia <natalia-321s@outlook.com>
2020-12-04 13:18:46 -0300

    Mensagens de sucesso/erro

--- Alura/Frontend/Javascript/fetchAPI/edita-cliente.js
+++ Alura/Frontend/Javascript/fetchAPI/edita-cliente.js
@@ -11,6 +11,30 @@ detalhaCliente(id).then(dados => {
 })
 
 const formEdicao = document.querySelector('[data-form]')
+
+const mensagemSucesso = (mensagem) => {
+    const linha = document.createElement('tr')
+
+    const conteudoLinha = `
+        <div class="alert alert-success" role="alert">
+            ${mensagem}
+        </div>
+    `
+    linha.innerHTML = conteudoLinha;
+    return linha
+}
+const mensagemErro = (mensagem) => {
+    const linha = document.createElement('tr')
+
+    const conteudoLinha = `
+        <div class="alert alert-warning" role="alert">
+            ${mensagem}
+        </div>
+    `
+    linha.innerHTML = conteudoLinha;
+    return linha
+}
+
 formEdicao.addEventListener('submit', event => {
     event.preventDefault()
 
@@ -19,5 +43,11 @@ formEdicao.addEventListener('submit', event => {
         return
     }
 
-    editaCliente(id, inputCPF.value, inputNome.value)
+    editaCliente(id, inputCPF.value, inputNome.value).then( response =>{
+        if(response.status === 200){
+            formEdicao.appendChild(mensagemSucesso("Cliente editado com sucesso!"))
+        } else {
+            formEdicao.appendChild(mensagemErro("Erro ao editar cliente."))
+        }
+    })
 })
